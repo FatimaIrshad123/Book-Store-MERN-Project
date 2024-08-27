f065724a697b1637f9d88ac4532e0d14268e38b0
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-08-27 22:23:26 +0500

    Working on backened

--- backened/index.ts
+++ backened/index.ts
@@ -1,1 +1,15 @@
-console.log('hi')
+import express from "express";
+import { PORT } from "./config";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.get('/', (req,res) => {
+    //console.log(req);
+    return res.status(200).send("Welcome to the home page")
+})
+app.listen(PORT, () => {
+    console.log(`App running on port: ${PORT}`)
+});
+
+mongoose.connect()
--- backened/src/index.js
+++ backened/src/index.js
@@ -1,2 +1,10 @@
-"use strict";
-console.log('hi');
+import express from "express";
+import { PORT } from "./config.js";
+const app = express();
+app.get('/', (req, res) => {
+    console.log(req);
+    return res.status(200).send("Welcome to the home page");
+});
+app.listen(PORT, () => {
+    console.log(`App running on port: ${PORT}`);
+});
