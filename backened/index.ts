4d7b8660c87ea621465c70a585fcb5ded40e5f00
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-08-28 22:01:14 +0500

    Added book model

--- backened/index.ts
+++ backened/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { PORT } from "./config";
+import { mongodbURL, PORT } from "./config";
 import mongoose from "mongoose";
 
 const app = express();
@@ -8,8 +8,13 @@ app.get('/', (req,res) => {
     //console.log(req);
     return res.status(200).send("Welcome to the home page")
 })
-app.listen(PORT, () => {
-    console.log(`App running on port: ${PORT}`)
-});
 
-mongoose.connect()
+
+mongoose.connect(mongodbURL).then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+        console.log(`App running on port: ${PORT}`)
+    });
+}).catch((error) => {
+    console.log('Error', error)
+})
