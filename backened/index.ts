6c677ac56cc4c6f0e78779ca9cda674c3faccd3d
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-08-30 20:39:39 +0500

    Added cors

--- backened/index.ts
+++ backened/index.ts
@@ -1,14 +1,18 @@
 import express from "express";
 import { mongodbURL, PORT } from "./config";
 import mongoose from "mongoose";
-import { Book } from "./models/bookModels";
 import booksRoute from "./routes/booksRoute";
-
+import cors from 'cors'
 const app = express();
 app.use(express.json())
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET','POST','PUT','DELETE'],
+    allowedHeaders: ['Content-Type']
+}));
+
 
 app.get('/', (req,res) => {
-    //console.log(req);
     return res.status(200).send("Welcome to the home page")
 })
 
